09344019b48729c6c847ad300d01034c59d850b5
Guilherme Lacerda <guiglacerda@gmail.com>
2017-11-07 16:57:27 -0200

    Adds indentention in C code

--- brainfk_translate.py
+++ brainfk_translate.py
@@ -3,19 +3,21 @@ from types import SimpleNamespace
 
 source =  '''
 HELLO BRAINFUCK!
-++++.
----.
-,>>>>
-+++.
----.
--.
++
+-
+-
++
++++++++,
+.
 '''
 
 ctx = SimpleNamespace(tokens=[], indent=0)
-ctx.tokens.append('int main() {\n')
-ctx.indent += 1
 
-print(''.join(ctx.tokens))
+ctx.tokens.append('#include "stdio.h"\n')
+ctx.tokens.append('#include "stdlib.h"\n')
+ctx.tokens.append('\nint main() {\n')
+
+ctx.indent += 1
 
 def bf(source):
     """
@@ -33,23 +35,23 @@ def bf(source):
 
         if cmd == '+':
             data[ptr] = (data[ptr] + 1) % 256
-            ctx.tokens.append('caracter[ptr]++;\n')
+            ctx.tokens.append(('    ' * ctx.indent) + 'caracter[ptr]++;\n')
         elif cmd == '-':
             data[ptr] = (data[ptr] - 1) % 256
-            ctx.tokens.append('caracter[ptr]--;\n')
+            ctx.tokens.append(('    ' * ctx.indent) + 'caracter[ptr]--;\n')
         elif cmd == '>':
             ptr += 1
-            ctx.tokens.append('ptr++;\n')
+            ctx.tokens.append(('    ' * ctx.indent) + 'ptr++;\n')
             if ptr == len(data):
                 data.append(0)
         elif cmd == '<':
             ptr -= 1
-            ctx.tokens.append('ptr--;\n')
+            ctx.tokens.append(('    ' * ctx.indent) + 'ptr--;\n')
         elif cmd == '.':
-            ctx.tokens.append('putchar(data[ptr]);\n')
+            ctx.tokens.append(('    ' * ctx.indent) + 'putchar(data[ptr]);\n')
         elif cmd == ',':
             data[ptr] = ord(getche())
-            ctx.tokens.append('data[ptr] = getchar(data[ptr]);\n')
+            ctx.tokens.append(('    ' * ctx.indent) + 'data[ptr] = getchar(data[ptr]);\n')
         elif cmd == '[':
             if data[ptr] == 0:
                 open_brackets = 1
@@ -67,11 +69,11 @@ def bf(source):
 
         code_ptr += 1
 
-        ctx.tokens.append('return 0;\n')
-        ctx.tokens.append('}\n')
 
-        print(''.join(ctx.tokens))
+    ctx.tokens.append('\n' + '    ' + 'return 0;\n')
+    ctx.tokens.append('}\n')
 
+    print(''.join(ctx.tokens))
 
     return data
 
